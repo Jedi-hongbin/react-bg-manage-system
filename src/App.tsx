3883d499deaba535da6de23953e21e0786bbef6f
Hongbin <2218176087@qq.com>
2021-01-31 19:59:27 +0800

    shadow card

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from "react";
 import { Button, Space, Row, Col } from "antd";
 import "./App.css";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
+import { Card } from "./components/UI/Card";
 
 const MyButton = styled.button`
   background-color: ${(props: { bgc?: string }) => props.bgc || "#51f"};
@@ -12,84 +13,7 @@ const MyButton = styled.button`
   color: white;
 `;
 
-// const shadow = css`
-//   box-shadow: ${({ bgc }: { bgc?: string }) =>
-//     `0px 0px 15px 5px ${bgc || "orange"}`};
-// `;
-const textShadow = css`
-  text-shadow: 1px 1px 1px tomato;
-`;
-
-const flexCenter = css`
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
-/* ${(props: { bgc?: string; center?: boolean }) => {
-    console.log("props:", props);
-    return props.center ? flexCenter : undefined;
-  }} */
-const Card = styled.div`
-  width: 200px;
-  height: 200px;
-  display: flex;
-  box-shadow: 0px 0px 10px 3px #ccc;
-  border-radius: 5px;
-  transition: 0.3s linear all;
-  background-color: ${({ bgc }: { bgc?: string }) => bgc || "orange"};
-
-  ${(props: { bgc?: string; center?: boolean }) =>
-    props.center ? flexCenter : undefined}
-
-  :hover {
-    box-shadow: ${({ bgc }: { bgc?: string }) =>
-      `0px 0px 15px 5px ${bgc || "orange"}`};
-  }
-
-  button {
-    background-color: ${({ bgc }: { bgc?: string }) => bgc || "orange"};
-    transition: 0.3s linear all;
-    :hover {
-      box-shadow: ${({ bgc }: { bgc?: string }) =>
-        `0px 0px 15px 5px ${bgc || "orange"}`};
-    }
-    :active {
-      background-color: ${({ bgc }: { bgc?: string }) => bgc || "orange"};
-    }
-    :nth-child(odd) {
-      color: blue;
-      background-color: #fff;
-      ${textShadow}
-    }
-  }
-`;
-console.log("Card:", Card);
-// const AntDButton = styled(Button)`
-//   background-color: ${({
-//     imp,
-//     bgc = "green",
-//   }: {
-//     imp?: boolean;
-//     bgc?: string;
-//   }) => {
-//     console.log("hello!");
-//     return `${bgc} ${imp ? "!important" : ""}`;
-//   }};
-// `;
-
-const redBackGround = css`
-  background-color: red !important;
-`;
-
-const AntDButton = styled(Button)`
-  && {
-    background-color: green;
-  }
-  background-color: black;
-  ${redBackGround}
-`;
-
-const ExtendCard = styled(Card)``;
+const AntDButton = styled(Button)``;
 
 const Input = styled.input`
   border: none;
@@ -106,8 +30,6 @@ const PassWorldInput = styled(Input).attrs({
 })`
   border-bottom: 2px solid #faa;
 `;
-
-const LinkCard = Card.withComponent("a");
 
 const border: any = { border: "1px solid orange", height: "50px" };
 
@@ -137,18 +59,24 @@ const App: FC = () => {
           <Col style={border} flex={1}></Col>
           <Col style={border} flex={2}></Col>
         </Row>
-        <Space style={{ border: "1px solid #000", width: "100%" }}>
-          <Card bgc="#afa" center>
-            <Button type="primary">Button</Button>
-            <MyButton>MyButton</MyButton>
-            <AntDButton type="ghost">AntDButton</AntDButton>
-          </Card>
-          <ExtendCard bgc="#f00" />
-          <ExtendCard bgc="#00f" />
+        <Space
+          style={{
+            border: "1px solid #000",
+            width: "100%",
+            overflowX: "scroll",
+          }}
+        >
           <AntDButton>AntDButton</AntDButton>
           <Input />
           <PassWorldInput />
-          <LinkCard />
+        </Space>
+        <Space>
+          <Card bgc="orange">
+            <span>orange</span>
+          </Card>
+          <Card bgc="blue">
+            <span>blue</span>
+          </Card>
         </Space>
       </Space>
     </div>
